cb6db86e5836e3a2d0ad5f55a4556531f0309312
VictorHL <adaw25vhuedo@inslaferreria.cat>
2026-09-23 16:57:09 +0200

    Primera modificacion Semana 2

--- cliente/script.js
+++ cliente/script.js
@@ -1,7 +1,22 @@
-const boto = document.getElementById("btnSaluda");
-boto.addEventListener("click", saluda);
-
 function saluda() {
   alert("Hola, crack!");
 }
+
+const boto = document.getElementById("btnSaluda");
+boto.addEventListener("click", saluda);
+
+
+
+const titol = document.querySelector("#titolPrincipal");
+
+titol.textContent = "📮 El Cartero Invisible - Setmana 2";
+titol.setAttribute("data-role", "banner");
+
+const contenidor = document.querySelector("#contenidorCartes");
+contenidor.innerHTML = "<p>Cartes pendents: 0</p>";
+
+const info = document.querySelector(".info");
+info.style.color = "#2c3e50";
+
+
 
